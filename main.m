a1988771af84e3bde0db8164396f553ef0be883d
moondewaudio <38087505+moondewaudio@users.noreply.github.com>
2021-12-12 09:33:47 -0800

    Add files via upload

--- main.m
+++ main.m
@@ -7,10 +7,10 @@ if ch ~= 1
 end
 
 numHarmAdj = 4; % # of harmonics to adjust
-bw = 75; % bandwidth of filters to adjust
-gain = 6; % dB gain wanted in those harmonics
-numVoices = 14; % how many voices per octave for the filter
-winTime = 0.75;
+bw = 75; % bandwidth of filters applied to the harmonics
+gain = 6; % dB gain wanted in those harmonics (can be negative)
+numVoices = 10; % how many voices per octave for the filter
+winTime = 0.75; % how long should the EQ latch for between switches
 
 output = cwtTrackEQ(x, fs, numHarmAdj, bw, gain, numVoices, winTime);
 
